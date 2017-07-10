f0f76bc7e2e396d9c9e9084ba0cbeebc6d76d81f
Julio722 <juliolopes722@gmail.com>
2017-07-10 06:06:06 -0300

    Update MODELO FÍSICO.sql

--- MODELO FÍSICO.sql
+++ MODELO FÍSICO.sql
@@ -1,31 +1,14 @@
--- Geração de Modelo físico
+-- GeraÃ§Ã£o de Modelo fÃ­sico
 -- Sql ANSI 2003 - brModelo.
 
 
 
-CREATE TABLE PROFESSOR (
-siape VARCHAR(10),
-horario_prof VARCHAR(10),
-cod_pessoa VARCHAR(10)
-)
-
-CREATE TABLE PESSOA (
-nome VARCHAR(10),
-senha VARCHAR(10),
-email VARCHAR(10),
-cod_pessoa VARCHAR(10) PRIMARY KEY
-)
-
 CREATE TABLE PAUTA (
+data VARCHAR(10),
 cod_pauta VARCHAR(10) PRIMARY KEY,
 situacao VARCHAR(10),
-data VARCHAR(10),
+cod_pessoa VARCHAR(10),
 cod_turma VARCHAR(10)
-)
-
-CREATE TABLE TURMA (
-descricao VARCHAR(10),
-cod_turma VARCHAR(10) PRIMARY KEY
 )
 
 CREATE TABLE DISCIPLINA (
@@ -42,30 +25,65 @@ cod_discip VARCHAR(10),
 FOREIGN KEY(cod_discip) REFERENCES DISCIPLINA (cod_discip)
 )
 
-CREATE TABLE ALUNO (
-horario_aluno VARCHAR(10),
+CREATE TABLE PESSOA_ALUNO_PROFESSOR (
+nome VARCHAR(10),
+senha VARCHAR(10),
+email VARCHAR(10),
+cod_pessoa VARCHAR(10) PRIMARY KEY,
 matricula VARCHAR(10),
+horario_do_aluno VARCHAR(10),
+siape VARCHAR(10),
+horario_do_professor VARCHAR(10)
+)
+
+CREATE TABLE TURMA (
+cod_turma VARCHAR(10) PRIMARY KEY,
+descricao VARCHAR(10)
+)
+
+CREATE TABLE HORARIO (
+horario_de_inicio VARCHAR(10),
+horario_de_fim VARCHAR(10),
+dia_da_semana VARCHAR(10),
+data VARCHAR(10),
+local VARCHAR(10),
+codigo_horario VARCHAR(10) PRIMARY KEY,
+cod_discip VARCHAR(10),
 cod_pessoa VARCHAR(10),
-FOREIGN KEY(cod_pessoa) REFERENCES PESSOA (cod_pessoa)
+FOREIGN KEY(cod_discip) REFERENCES DISCIPLINA (cod_discip),
+FOREIGN KEY(cod_pessoa) REFERENCES PESSOA_ALUNO_PROFESSOR (cod_pessoa)
+)
+
+CREATE TABLE REALIZA (
+NOTA VARCHAR(10),
+cod_atvd VARCHAR(10),
+cod_pessoa VARCHAR(10),
+FOREIGN KEY(cod_atvd) REFERENCES ATIVIDADE (cod_atvd),
+FOREIGN KEY(cod_pessoa) REFERENCES PESSOA_ALUNO_PROFESSOR (cod_pessoa)
 )
 
 CREATE TABLE AVALIA (
+nota VARCHAR(10),
 cod_turma VARCHAR(10),
-FOREIGN KEY(cod_turma) REFERENCES TURMA (cod_turma)
+cod_pessoa VARCHAR(10),
+FOREIGN KEY(cod_turma) REFERENCES TURMA (cod_turma),
+FOREIGN KEY(cod_pessoa) REFERENCES PESSOA_ALUNO_PROFESSOR (cod_pessoa)
 )
 
 CREATE TABLE disciplina_professor (
 cod_discip_prof VARCHAR(10),
+cod_pessoa VARCHAR(10),
 cod_discip VARCHAR(10),
 cod_pauta VARCHAR(10),
-PRIMARY KEY(cod_discip_prof,cod_discip,cod_pauta)
+PRIMARY KEY(cod_discip_prof,cod_pessoa,cod_discip,cod_pauta)
 )
 
-CREATE TABLE CONTÉM (
-NOTA VARCHAR(10),
-cod_atvd VARCHAR(10),
-FOREIGN KEY(cod_atvd) REFERENCES ATIVIDADE (cod_atvd)
+CREATE TABLE TEM (
+codigo_horario VARCHAR(10),
+cod_pauta VARCHAR(10),
+FOREIGN KEY(codigo_horario) REFERENCES HORARIO (codigo_horario),
+FOREIGN KEY(cod_pauta) REFERENCES PAUTA (cod_pauta)
 )
 
-ALTER TABLE PROFESSOR ADD FOREIGN KEY(cod_pessoa) REFERENCES PESSOA (cod_pessoa)
+ALTER TABLE PAUTA ADD FOREIGN KEY(cod_pessoa) REFERENCES PESSOA_ALUNO_PROFESSOR (cod_pessoa)
 ALTER TABLE PAUTA ADD FOREIGN KEY(cod_turma) REFERENCES TURMA (cod_turma)
